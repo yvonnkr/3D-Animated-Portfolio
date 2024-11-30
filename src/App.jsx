7bc16be31f303f2addd540bcb07f213ac4ad878d
yvonnkr <yvonnkr86@gmail.com>
2024-11-30 18:20:37 +0000

    setup project layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import Hero from "./components/hero/Hero.jsx";
+import Services from "./components/services/Services.jsx";
+import Portfolio from "./components/portfolio/Portfolio.jsx";
+import Contact from "./components/contact/Contact.jsx";
+
 const App = () => {
     return (
-        <section>
-            <div className="text-center">3D App!!</div>
-        </section>
+        <div className="container">
+            <section id="#home">
+                <Hero />
+            </section>
+            <section id="#services">
+                <Services />
+            </section>
+            <section id="#portfolio">
+                <Portfolio />
+            </section>
+            <section id="#contact">
+                <Contact />
+            </section>
+        </div>
     );
 };
 
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,6 @@
+import "./portfolio.css";
+
+const Portfolio = () => {
+    return <div className="portfolio">Portfolio</div>;
+};
+export default Portfolio;
